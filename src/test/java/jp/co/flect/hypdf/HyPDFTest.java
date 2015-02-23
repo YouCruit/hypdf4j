4f7e5f534fa2dca8bba4ea730794a60fdd7686f4
christianekstrand <christianekstrand@gmail.com>
2015-02-23 13:16:59 +0100

    dont catch and call fail in tests, let the test runner catch the exceptions and log it instead.

--- src/test/java/jp/co/flect/hypdf/HyPDFTest.java
+++ src/test/java/jp/co/flect/hypdf/HyPDFTest.java
@@ -3,7 +3,6 @@ package jp.co.flect.hypdf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -62,127 +61,84 @@ public class HyPDFTest {
 	}
 
 	@Test
-	public void loginError() {
-		log("loginError");
+	public void loginError() throws IOException {
 		try {
 			HyPDF hypdf = createHyPDF("", "");
 			hypdf.htmlToPdf("dummy");
-			fail();
 		} catch (HyPDFException e) {
 			log("loginError response: " + e.getStatusCode() + ", " + e.getMessage());
 			assertEquals(401, e.getStatusCode());
 			assertNotNull(e.getMessage());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
 		}
 	}
 
 	@Test
-	public void simpleHtml() {
-		long t = System.currentTimeMillis();
-		log("simpleHtml");
-		HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
-		String content = "http://flect-panda.herokuapp.com/public/fontface.html";
-		File file = new File(OUTPUT_DIR, "simpleHtml.pdf");
-		try {
-			HtmlToPdf option = new HtmlToPdf();
-			Footer footer = new Footer();
-			footer.center = "FLECT";
-			option.footer = footer;
+	public void simpleHtml() throws IOException {
+	    HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
+	    String content = "http://flect-panda.herokuapp.com/public/fontface.html";
+	    File file = new File(OUTPUT_DIR, "simpleHtml.pdf");
+	    HtmlToPdf option = new HtmlToPdf();
+	    Footer footer = new Footer();
+	    footer.center = "FLECT";
+	    option.footer = footer;
 
-			PdfResponse res = hypdf.htmlToPdf(content, option);
-			res.writeTo(file);
+	    PdfResponse res = hypdf.htmlToPdf(content, option);
+	    res.writeTo(file);
 
-			System.out.println("pdfInfo: " + res.getPages() + ", " + res.getPageSize() + ", " + res.getPdfVersion());
-			assertEquals(1, res.getPages());
-			assertTrue(res.getPageSize().indexOf("letter") >= 0);
-			assertEquals("1.4", res.getPdfVersion());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		} finally {
-			System.out.println("Time: " + (System.currentTimeMillis() - t) + "ms");
-		}
+	    System.out.println("pdfInfo: " + res.getPages() + ", " + res.getPageSize() + ", " + res.getPdfVersion());
+	    assertEquals(1, res.getPages());
+	    assertTrue(res.getPageSize().indexOf("letter") >= 0);
+	    assertEquals("1.4", res.getPdfVersion());
 	}
 
 	@Test
-	public void callback() {
-		log("callback");
-		HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
-//		String content = "https://excel-report2.herokuapp.com/report/html/sample/chart?score=50&score=40&score=60&score=70&score=80&score=82&score=73&score=94&score=100&score=62&score=72&score=43&score=22&score=84&score=63&app.print=false&app.download=false&app.webfont=true&app.border=false";
-		String content = "http://flect-panda.herokuapp.com/public/fontface.html";
-		try {
-			URL url = new URL("http://flect-panda.herokuapp.com/");
-			String jobId = hypdf.notifyHtmlToPdf(content, url);
-			System.out.println("job_id: " + jobId);
-			assertNotNull(jobId);
-			JobStatus status = hypdf.jobstatus(jobId);
-			System.out.println("status: " + status);
-			assertNotNull(status);
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		}
+	public void callback() throws IOException {
+	    HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
+	    //		String content = "https://excel-report2.herokuapp.com/report/html/sample/chart?score=50&score=40&score=60&score=70&score=80&score=82&score=73&score=94&score=100&score=62&score=72&score=43&score=22&score=84&score=63&app.print=false&app.download=false&app.webfont=true&app.border=false";
+	    String content = "http://flect-panda.herokuapp.com/public/fontface.html";
+	    URL url = new URL("http://flect-panda.herokuapp.com/");
+	    String jobId = hypdf.notifyHtmlToPdf(content, url);
+	    System.out.println("job_id: " + jobId);
+	    assertNotNull(jobId);
+	    JobStatus status = hypdf.jobstatus(jobId);
+	    System.out.println("status: " + status);
+	    assertNotNull(status);
 	}
 
 	@Test
-	public void pdfInfo() {
-		log("pdfInfo");
-		HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
-		File file = new File(INPUT_DIR, "simpleHtml.pdf");
-		try {
-			Map<String, Object> ret = hypdf.pdfInfo(file);
-			System.out.println(ret);
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		}
+	public void pdfInfo() throws IOException {
+	    log("pdfInfo");
+	    HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
+	    File file = new File(INPUT_DIR, "simpleHtml.pdf");
+	    Map<String, Object> ret = hypdf.pdfInfo(file);
+	    System.out.println(ret);
 	}
 
 	@Test
-	public void pdfToTest() {
-		log("pdfToText");
-		HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
-		File file = new File(INPUT_DIR, "simpleHtml.pdf");
-		try {
-			Map<String, Object> ret = hypdf.pdfToText(file);
-			System.out.println(ret);
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		}
+	public void pdfToTest() throws IOException {
+	    HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
+	    File file = new File(INPUT_DIR, "simpleHtml.pdf");
+	    Map<String, Object> ret = hypdf.pdfToText(file);
+	    System.out.println(ret);
 	}
 
 	@Test
-	public void pdfUnite() {
-		log("pdfUnite");
-		HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
-		File input = new File(INPUT_DIR, "simpleHtml.pdf");
-		File output = new File(OUTPUT_DIR, "pdfUnite.pdf");
-		try {
-			PdfResponse res = hypdf.pdfUnite(input, input, input);
-			res.writeTo(output);
-			//assertEquals(3, res.getPages());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		}
+	public void pdfUnite() throws IOException {
+	    HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
+	    File input = new File(INPUT_DIR, "simpleHtml.pdf");
+	    File output = new File(OUTPUT_DIR, "pdfUnite.pdf");
+	    PdfResponse res = hypdf.pdfUnite(input, input, input);
+	    res.writeTo(output);
+	    //assertEquals(3, res.getPages());
 	}
 
 	@Test
-	public void pdfExtract() {
-		log("pdfExtract");
-		HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
-		File input = new File(INPUT_DIR, "pdfUnite.pdf");
-		File output = new File(OUTPUT_DIR, "pdfExtract.pdf");
-		try {
-			PdfResponse res = hypdf.pdfExtract(input, 2, 2);
-			res.writeTo(output);
-			//assertEquals(3, res.getPages());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		}
+	public void pdfExtract() throws IOException {
+	    HyPDF hypdf = createHyPDF(USERNAME, PASSWORD);
+	    File input = new File(INPUT_DIR, "pdfUnite.pdf");
+	    File output = new File(OUTPUT_DIR, "pdfExtract.pdf");
+	    PdfResponse res = hypdf.pdfExtract(input, 2, 2);
+	    res.writeTo(output);
+	    //assertEquals(3, res.getPages());
 	}
 }
